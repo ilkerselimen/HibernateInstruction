6b69d655e47c2cb46f8738193b1a3c241440bb18
İlker Şelimen <ilkerselimen@gmail.com>
2023-03-22 22:28:25 +0300

    v02

--- src/main/java/com/hb01/annotation/Student01.java
+++ src/main/java/com/hb01/annotation/Student01.java
@@ -1,18 +1,28 @@
 package com.hb01.annotation;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity//!!! @Entity ile Bu sinifin DB de bir tabloya karsilik gelmesini sagliyoruz, student01
 @Table(name="t_student01") //!!! DB de tablo isminin "t_student01" olarak degismesini sagladim.
-//opsiyoneldir, tablo adi konulmazsa class adini tablo adi olarak default alir
+//opsiyoneldir, tablo adi konulmazsa class adini tablo adi olarak default alir.
+//Java kodu içinde bu class'a ulaşırken Student01 ile, SQL ile ulaşirken t_student01 ile yazmam lazım
 public class Student01 {
 
+    @Id // !!! primary-key olusmasini sagliyoruz
+    //@Column(name="std_id")
     private int id;
+
+    @Column(name="student_name", length = 100, nullable = false, unique = false)
     private String name;
+
+    //@Transient // !!! DB deki tabloda "grade" adinda bir column olusmasini engeller.
     private int grade;
 
+//    @Lob // !!! large object ile buyuk boyutlu datalar tutulabiliyor.
+//    private byte[] image;
+
     // !!! GETTER - SETTER
+
     public int getId() {
         return id;
     }
