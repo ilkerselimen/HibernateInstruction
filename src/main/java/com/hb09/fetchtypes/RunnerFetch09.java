7bdcac250925a32aa3dafc2ed8ff0c7dfdf6993d
İlker Şelimen <ilkerselimen@gmail.com>
2023-03-30 22:51:59 +0300

    v09

---
+++ src/main/java/com/hb09/fetchtypes/RunnerFetch09.java
@@ -0,0 +1,29 @@
+package com.hb09.fetchtypes;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerFetch09 {
+
+    public static void main(String[] args) {
+
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Student09.class).addAnnotatedClass(Book09.class);
+
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+
+
+
+        tx.commit();
+
+        session.close();
+
+        sf.close();
+
+    }
+}
