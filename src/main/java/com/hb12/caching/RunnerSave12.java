c2497ef779ae007a55472b8fb773e916464f01b3
İlker Şelimen <ilkerselimen@gmail.com>
2023-04-04 15:47:01 +0300

    v12

--- src/main/java/com/hb12/caching/RunnerSave12.java
+++ src/main/java/com/hb12/caching/RunnerSave12.java
@@ -1,13 +1,11 @@
 package com.hb12.caching;
 
-import com.hb11.criteriaapi.Student11;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class RunnerSave12 {
-
     public static void main(String[] args) {
 
         Student12 student1 = new Student12();
@@ -24,6 +22,7 @@ public class RunnerSave12 {
 
         Configuration con = new Configuration().configure("hibernate.cfg.xml").
                 addAnnotatedClass(Student12.class);
+
         SessionFactory sf = con.buildSessionFactory();
         Session session = sf.openSession();
         Transaction tx = session.beginTransaction();
@@ -37,5 +36,4 @@ public class RunnerSave12 {
         sf.close();
 
     }
-
 }
