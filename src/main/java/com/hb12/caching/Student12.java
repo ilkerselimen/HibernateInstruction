bb15b02eccce7984caa443ce795aa9b1f0b470c0
İlker Şelimen <ilkerselimen@gmail.com>
2023-04-04 15:46:53 +0300

    v12

--- src/main/java/com/hb12/caching/Student12.java
+++ src/main/java/com/hb12/caching/Student12.java
@@ -1,43 +1,48 @@
 package com.hb12.caching;
 
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Cache;
+
 import javax.persistence.*;
 
 /*
-1)  First Level Cache --->
-          * defaultta açık geliyor , kapatma durumu yok
-          * Aynı session içinde kayıt alır
-          * session kapanınca içindejkiler silinir
+1)	First Level Cache --->
+			* defaultta açık geliyor , kapatma durumu yok
+			* Aynı session içinde kayıt alır
+			* session kapanınca içindejkiler silinir
 2) Second Level Cache --->
-          * Defaultta kapalıdır
-          * Session factory seviyesinde cachleme yapar, yani farklı
-                sessionlar arasında data kullanılabiliyor
-          * hibernate.cfg.xml den active edilebilir
+			* Defaultta kapalıdır
+			* Session factory seviyesinde cachleme yapar, yani farklı
+					sessionlar arasında data kullanılabiliyor
+			* hibernate.cfg.xml den active edilebilir
 3) Query Cache
-          * Query ler için kullanılıyor
-          * hibernate.cfg.xml den active edilebilir
+			* Query ler için kullanılıyor
+			* hibernate.cfg.xml den active edilebilir
  */
 
 @Entity
+@Cacheable
+@Cache(usage= CacheConcurrencyStrategy.READ_WRITE, region = "Student12")
 public class Student12 {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "student_name", nullable = false)
+    @Column(name="student_name", nullable = false)
     private String name;
 
-    private int mathGrade;
+    private int mathGrade ;
 
-    // !!! Getter - Setter
+    // !!! Getter -Setter
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
 
     public String getName() {
         return name;
@@ -65,5 +70,4 @@ public class Student12 {
                 ", mathGrade=" + mathGrade +
                 '}';
     }
-
 }
